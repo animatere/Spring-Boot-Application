84a92bea3776c132abc69b06dd5d613df0ca04d4
animatere <animatere@gmail.com>
2021-02-04 15:04:14 +0100

    Added Api

--- src/main/java/com/example/SpringBootApplication/Application.java
+++ src/main/java/com/example/SpringBootApplication/Application.java
@@ -1,14 +1,28 @@
 package com.example.SpringBootApplication;
 
+import com.example.SpringBootApplication.Api.User;
+import com.example.SpringBootApplication.Api.UserController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
 	}
 
 
+	@Bean
+	CommandLineRunner runner(UserController userController) {
+
+		return args -> {
+			userController.save(new User("test", "test", "test@outlook.de"));
+		};
+
+	}
 }
